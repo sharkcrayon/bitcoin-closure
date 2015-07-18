67412e61c90f4c2255b9bc54297cd153138caccb
Serena Randolph <serena.randolph@gmail.com>
2015-07-17 19:10:18 -0700

    Cleaning up code.

--- js/site.js
+++ js/site.js
@@ -30,82 +30,6 @@ $(document).ready(function() {
         $('#results > pre').text(message + JSON.stringify(data, null, '\t'));
     };
 
-    // var extractBTCAddresses = function(txn, data, addr) {
-    //     displayData(data, 'BTC Addresses: ');
-    //     //in particular we are interested in the data.vin section (data.vin is an array, where each array entry corresponds to exactly one of the inputs of the transaction txn)
-    //     //if there is only one input, then we don't need to do anything more.
-    //     // console.log("           The number of inputs in this txn is: ", data.vin.length, ". They are: ", data.vin);
-    //     if (data.vin.length > 1){
-    //         // console.log("           Since there is more than 1 input we look at the inputs.")
-    //         //now for each input in data.vin we must extract the btc address of the input...
-    //         inputsListBtcAddresses = [];
-
-    //         for (var inputnum = 0; inputnum < data.vin.length; inputnum++){
-    //             //extract the btc address of the inputsList and add it to inputsListBtcAddresses
-    //             inputsListBtcAddresses = inputsListBtcAddresses.concat( data.vin[inputnum].addr );
-    //             // console.log("               Input ", inputnum, " added. inputsListBtcAddresses looks like: ", inputsListBtcAddresses)
-    //         }
-    //         // console.log("               Finished adding input btc address for txn ", txn, "The list looks like: ", inputsListBtcAddresses)
-    //         // console.log("               Now we check to see if addr (", addr, ") is among them.")
-    //         //now all input addresses are stored in inputsListBtcAddresses.
-    //         //we need to check to see if addr is in inputsListBtcAddresses. 
-    //         //If it's not then we're done (because we are only looking for new addresses that have been inputs along with addr).
-    //         //If it is, then all addresses in inputsListBtcAddresses are in the closure of bitaddress.
-    //         var found_addr = false;
-    //         var non_addr_addreses = [];
-
-    //         for (var inputaddress in inputsListBtcAddresses) {
-    //             if (inputaddress == addr) {
-    //                 found_addr = true;
-    //                 // console.log("                Found addr among the input addresses. So the other addresses must be in the closure.")
-    //             } else {
-    //                 non_addr_addreses = non_addr_addreses.concat(inputaddress);
-
-    //             }
-    //         }
-
-    //         if (found_addr){
-    //             //so if this code executes, the list non_addr_addreses is in the closure of bitaddress.
-    //             //so we check all the addresses in this list to see if (1) it's already in the list closure or (2) it's already in the list toBeProcessed.
-    //             //if neither of those things are true, then we add the address to the list toBePrcessed.
-    //             // console.log("                   Before we add an address to the closure we check to see if we've already added it there previously.");
-                
-    //             for (var newaddr in non_addr_addreses){
-    //                 var inclosure = false;
-    //                 for (var c in closure){
-    //                     if (newaddr == c){
-    //                         inclosure = true;
-    //                         // console.log("                   The address ", c, " has already been added to the closure.");
-    //                         break;
-    //                     }
-    //                 }
-    //                 if (!inclosure){
-    //                     // console.log("                   The address ", c, "has not yet been added to the closure. \n let's check to see if it's in the toBeProcessed list.");
-    //                     var intoBeProcessed = false;
-    //                     for (var p in toBeProcessed){
-    //                         if (p == newaddr){
-    //                             intoBeProcessed = true;
-    //                             // console.log("                       It is in the toBeProcessed list already.");
-    //                             break;
-    //                         }
-    //                     }
-    //                     //if this code execute then newddt is NOT in closure and NOT in toBeProcessed.
-    //                     //So we need to add it to the list toBeProcessed.
-    //                     // console.log("               The address ", c, "is not in the toBeProcessed list either. So we'll add it there.")
-    //                     toBeProcessed = toBeProcessed.concat(newaddr);
-    //                     // console.log("The toBeProcessed list looks like this now: ", toBeProcessed);
-    //                 }
-    //             }
-    //         }
-    //         else{
-    //             // console.log("The address ", addr, "was not found among them, so we don't add these input address to the closure. Moving on to the next txn.")
-    //         }
-    //     }
-    //     inputsList = data.vin;
-    // };
-
-// ================================================================================ //
-
     var iterationTracker = {
         'getTransactionHashes' : {
             'iterations' : 0,
@@ -179,7 +103,7 @@ $(document).ready(function() {
                     }
                 }
             }).then(function() {
-                console.log('we received the transactions');
+                console.log('we received the transaction');
                 iterationTracker.getTransactions.iterations++;
                 if (isFinished('getTransactions')) {
                     console.log('we have all the transactions');
@@ -188,6 +112,7 @@ $(document).ready(function() {
                         document.dispatchEvent(transactionsReceived);
                     } else {
                         console.log('there are no more transactions that need to be processed.');
+                        console.log('closure: ', closure);
                     }
                 }
             });
@@ -211,8 +136,7 @@ $(document).ready(function() {
                 url : 'https://insight.bitpay.com/api/addr/' + addr + '?format=json',
                 success : function(data) {
                     displayData(data.transactions, 'Transactions associated with original address: \n\n');
-                    txnList = data.transactions; // store the transaction data for further processing
-                    numberTxn = txnList.length;
+                    txnList = txnList.concat(data.transactions); // store the transaction data for further processing
                 }
             }).then(function() {
                 console.log('we received the hash');
@@ -232,7 +156,9 @@ $(document).ready(function() {
         // do nice things for the user
         initSubmit();
 
-        bitaddress = '1PhxUNNLFgMALYQv1UhHRnkc4aukos9vFL'; // '1PabtoJrSJmDDTf3v5KzM1c4SK2kpkmUnt'; // $('#f-bitaddress__input').val(); //EXAMPLE WITH A COINJOIN TRANSACTION bitaddress = '1CAbbXyRpdtpA6TKXss2Ydd1gWfPGyCJdK';
+        // TODO: Check that entered value is actually a bitcoin address.
+
+        bitaddress = '1CAbbXyRpdtpA6TKXss2Ydd1gWfPGyCJdK'; // $('#f-bitaddress__input').val(); //EXAMPLE WITH A COINJOIN TRANSACTION bitaddress = '1CAbbXyRpdtpA6TKXss2Ydd1gWfPGyCJdK';
         toBeProcessed.push(bitaddress);
 
         console.log('getting transaction hashes');
